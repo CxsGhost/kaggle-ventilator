f3ee330a6bd16ba93c1cee30a7d9f98e2de2cd27
CxsGhost <1659101919@qq.com>
2021-10-13 16:27:47 +0800

    2

--- notebooks/data_preprocessing.py
+++ notebooks/data_preprocessing.py
@@ -1,3 +1,5 @@
+#%%
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -12,7 +14,7 @@ def add_features(df):
     df['u_in_cumsum'] = (df['u_in']).groupby(df['breath_id']).cumsum()
 
     df['u_in_lag1'] = df.groupby('breath_id')['u_in'].shift(1)
-    df['u_out_lag1'] = df.groupby('breath_id')['u_out'].shift(1)
+    # df['u_out_lag1'] = df.groupby('breath_id')['u_out'].shift(1)
     # df['u_in_lag_back1'] = df.groupby('breath_id')['u_in'].shift(-1)
     # df['u_out_lag_back1'] = df.groupby('breath_id')['u_out'].shift(-1)
     # df['u_in_lag2'] = df.groupby('breath_id')['u_in'].shift(2)
@@ -30,12 +32,12 @@ def add_features(df):
     df = df.fillna(0)
 
     df['breath_id__u_in__max'] = df.groupby(['breath_id'])['u_in'].transform('max')
-    df['breath_id__u_out__max'] = df.groupby(['breath_id'])['u_out'].transform('max')
+    # df['breath_id__u_out__max'] = df.groupby(['breath_id'])['u_out'].transform('max')
 
     df['u_in_diff1'] = df['u_in'] - df['u_in_lag1']
-    df['u_out_diff1'] = df['u_out'] - df['u_out_lag1']
+    # df['u_out_diff1'] = df['u_out'] - df['u_out_lag1']
     df.drop(['u_in_lag1'], axis=1)
-    df.drop(['u_out_lag1'], axis=1)
+    # df.drop(['u_out_lag1'], axis=1)
     # df['u_in_diff2'] = df['u_in'] - df['u_in_lag2']
     # df['u_out_diff2'] = df['u_out'] - df['u_out_lag2']
 
@@ -57,14 +59,25 @@ def add_features(df):
 
 
 train = add_features(train_ori)
+#%%
+
 targets = train[['pressure']].to_numpy().reshape(-1, 80)
 train.drop(['pressure', 'id', 'breath_id'], axis=1, inplace=True)
 train = train.values
+#%%
+
+train_x_other = train[:, :-15]
+train_x_rc = train[:, -15:]
+#%%
 
 scale = StandardScaler()
-train = scale.fit_transform(train)
-train = train.reshape(-1, 80, train.shape[-1])
+scale.fit(train_x_other)
+train_x_other = scale.transform(train_x_other)
+train_x_other = train_x_other.reshape(-1, 80, train_x_other.shape[-1])
+train_x_rc = train_x_rc.reshape(-1, 80, train_x_rc.shape[-1])
 
-np.save('../data/x_train.npy', train)
-np.save('../data/y_train.npy', targets)
+#%%
 
+np.save('../data/train_x_rc.npy', train_x_rc)
+np.save('../data/train_x_other.npy', train_x_other)
+np.save('../data/target.npy', targets)
